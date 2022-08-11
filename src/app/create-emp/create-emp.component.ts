8efae5d7cf8be58364279ab1691a40948dc90419
Edward <Edward.Masih@ibm.com>
2022-08-11 18:18:40 +0530

    employee table created, validatoin, inputs remains

--- src/app/create-emp/create-emp.component.ts
+++ src/app/create-emp/create-emp.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { EmployeeService } from './create-emp.service';
+import { Employee } from './employee';
 
 @Component({
   selector: 'app-create-emp',
   templateUrl: './create-emp.component.html',
-  styleUrls: ['./create-emp.component.scss']
+  styleUrls: ['./create-emp.component.scss'],
 })
 export class CreateEmpComponent implements OnInit {
+  employees: Employee[] = [];
 
-  constructor() { }
+  // @ViewChild('traineeID', { static: true }) traineeID!: ElementRef;
 
-  ngOnInit(): void {
+  constructor(private empService: EmployeeService) {}
+
+  async ngOnInit() {
+    await this.empService.getAll().subscribe((data: Employee[]) => {
+      this.employees = data;
+      console.log(this.employees);
+    });
   }
 
+  traineeForm = new FormGroup({
+    name: new FormControl(),
+    degree: new FormControl(),
+  });
+
+  async addTrainee(trainee: any) {
+    console.log(trainee.value);
+    let newTrainee = { id: this.employees.length, ...trainee.value };
+    console.log(newTrainee);
+    await this.empService.createEmployee(newTrainee).subscribe((data: any) => {
+      console.log(data);
+      this.ngOnInit();
+      this.traineeForm.controls.name.setValue('');
+      this.traineeForm.controls.degree.setValue('');
+    });
+  }
 }
---
+++ src/app/create-emp/create-emp.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Employee } from './employee';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeService {
+  private apiServer = 'http://localhost:5000/employees';
+
+  constructor(private httpClient: HttpClient) {}
+
+  errorhandler(error: any) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // client side error
+      errorMessage = error.error.message;
+    } else {
+      // server side error
+      errorMessage = `Error code : ${error.status} \n Message : ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+
+  getAll(): Observable<Employee[]> {
+    return this.httpClient
+      .get<Employee[]>(this.apiServer)
+      .pipe(catchError(this.errorhandler));
+  }
+  createEmployee(emp: Employee): Observable<any> {
+    return this.httpClient
+      .post<Employee>(this.apiServer, emp)
+      .pipe(catchError(this.errorhandler));
+  }
+
+}
