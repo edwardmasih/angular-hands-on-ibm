948b56592c97149e53bcd8abd132530045ed9dfb
Edward <Edward.Masih@ibm.com>
2022-08-11 15:35:55 +0530

    working on dashboard carousel

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { DashboardService } from './dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  user: any = sessionStorage.getItem('email');
+  productList = [];
 
-  constructor() { }
+  constructor(private dashboardService: DashboardService) {}
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    await this.dashboardService.getAllData().subscribe((data: any) => {
+      console.log(data);
+      this.productList = data;
+    });
   }
-
 }
--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DashboardService {
+  private apiServer = 'https://fakestoreapi.com/products/';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  errorhandler(error: any) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // client side error
+      errorMessage = error.error.message;
+    } else {
+      // server side error
+      errorMessage = `Error code : ${error.status} \n Message : ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+
+  getAllData(): Observable<[]> {
+    return this.httpClient
+      .get<[]>(this.apiServer)
+      .pipe(catchError(this.errorhandler));
+  }
 }
